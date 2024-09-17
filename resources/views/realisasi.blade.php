0d79b5ff610f7c3b6bc4da8329ed6ec3e0d18696
windysab <windysabtami85@gmail.com>
2024-09-17 15:19:02 +0800

    git pull

--- resources/views/realisasi.blade.php
+++ resources/views/realisasi.blade.php
@@ -7,6 +7,7 @@
     <title>Chart</title>
     <link rel="stylesheet" href="{{ asset('css/stylesrealisasi.css') }}">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
 </head>
 
 <body>

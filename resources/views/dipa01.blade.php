0175938c5dcaec8b0be9105a42dabad93635c92b
windysab <windysabtami85@gmail.com>
2024-09-20 11:47:10 +0800

    oke lagi

--- resources/views/dipa01.blade.php
+++ resources/views/dipa01.blade.php
@@ -8,6 +8,7 @@
     <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+    <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
     <style>
         body {
             font-family: 'Roboto', sans-serif;
@@ -215,6 +216,20 @@
             },
             options: {
                 plugins: {
+                    datalabels: {
+                        formatter: function(value, context) {
+                            var total = context.chart.data.datasets[0].data.reduce((a, b) => a + b, 0);
+                            var percentage = (value / total * 100).toFixed(1);
+                            return percentage + '%';
+                        },
+                        color: '#fff',
+                        font: {
+                            size: 20,
+                            weight: 'bold'
+                        },
+                        anchor: 'center',
+                        align: 'center'
+                    },
                     tooltip: {
                         callbacks: {
                             label: function(context) {
@@ -223,15 +238,6 @@
                                 return label + ': ' + value;
                             }
                         }
-                    },
-                    doughnutlabel: {
-                        labels: [{
-                            text: '{{ $progressGajiPagu }}%',
-                            font: {
-                                size: 20,
-                                weight: 'bold'
-                            }
-                        }]
                     }
                 }
             }
@@ -249,6 +255,20 @@
             },
             options: {
                 plugins: {
+                    datalabels: {
+                        formatter: function(value, context) {
+                            var total = context.chart.data.datasets[0].data.reduce((a, b) => a + b, 0);
+                            var percentage = (value / total * 100).toFixed(1);
+                            return percentage + '%';
+                        },
+                        color: '#fff',
+                        font: {
+                            size: 20,
+                            weight: 'bold'
+                        },
+                        anchor: 'center',
+                        align: 'center'
+                    },
                     tooltip: {
                         callbacks: {
                             label: function(context) {
@@ -257,15 +277,6 @@
                                 return label + ': ' + value;
                             }
                         }
-                    },
-                    doughnutlabel: {
-                        labels: [{
-                            text: '{{ $progressOperasionalPagu }}%',
-                            font: {
-                                size: 20,
-                                weight: 'bold'
-                            }
-                        }]
                     }
                 }
             }

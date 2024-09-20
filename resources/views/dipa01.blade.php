d733b86069e1dd8a3f0d389ed40b57af7ce17234
windysab <windysabtami85@gmail.com>
2024-09-20 11:59:15 +0800

    oke

--- resources/views/dipa01.blade.php
+++ resources/views/dipa01.blade.php
@@ -9,6 +9,7 @@
     <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css">
     <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
     <script src="https://cdn.jsdelivr.net/npm/chartjs-plugin-datalabels"></script>
+    
     <style>
         body {
             font-family: 'Roboto', sans-serif;
